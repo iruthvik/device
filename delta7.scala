a08ac312af5cc92c15d35be6b90ebc87400c3d5a
iruthvik <51363006+iruthvik@users.noreply.github.com>
2023-10-05 23:52:08 +0530

    Update delta7.scala

--- delta7.scala
+++ delta7.scala
@@ -148,3 +148,39 @@ def calculateDelta(df: DataFrame, columns: List[String], windowDuration: String,
   deltaDF
 }
 
+----------------------
+
+--------------
+
+import org.apache.spark.sql.functions._
+
+def calculateDelta(df: org.apache.spark.sql.DataFrame, columns: List[String], windowDuration: String, watermarkDuaration: String): org.apache.spark.sql.DataFrame = {
+  val maxColumnsExpr = columns.map(colName => expr(s"max($colName) as max_$colName"))
+  val minColumnsExpr = columns.map(colName => expr(s"min($colName) as min_$colName"))
+  val allColumnsExper = maxColumnsExpr ++ minColumnsExpr
+
+  val aggregatedDF = df
+    .withWatermark("timestamp", watermarkDuaration)
+    .groupBy(
+      window(col("timestamp"), windowDuration, "5 minutes"), // Modified window definition
+      col("devicename"),
+      col("interface")
+    )
+    .agg(allColumnsExper.head, allColumnsExper.tail: _*)
+
+  val deltaColumns = columns :+ "window" // including for delta time
+
+  val deltaDF = deltaColumns.foldLeft(aggregatedDF) { (df, colName) =>
+    val deltaColumn = col(s"max_$colName") - col(s"min_$colName")
+    if (colName == "window") {
+      df.withColumn("delta_time", expr("UNIX_TIMESTAMP(window.end) - UNIX_TIMESTAMP(window.start)"))
+    } else {
+      df.withColumn(s"delta_$colName", deltaColumn)
+    }
+  }
+  deltaDF.drop("window") // Drop the 'window' column if you don't need it
+}
+
+val deltaDF = calculateDelta(finalDF, List("in_octets","out_octets","in_packets","out_packets"), "5 minutes", "5 minutes")
+deltaDF.show()
+
