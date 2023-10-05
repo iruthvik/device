66344258bd7b53df15e41c6406e0291ed6c613e6
iruthvik <51363006+iruthvik@users.noreply.github.com>
2023-10-05 15:35:00 +0530

    Create Delta5.scala

---
+++ Delta5.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{col, first, last, unix_timestamp}
+
+// Assuming you have a DataFrame named 'df' with 'timestamp' and 'bytes' columns
+
+// Create a window specification partitioned by 5-minute intervals
+val windowSpec = Window.orderBy("timestamp").rangeBetween(-4 * 60, 0)
+
+// Calculate the sum of bytes for the 5th record and 1st record in the window
+val fifthBytes = last(col("bytes")).over(windowSpec)
+val firstBytes = first(col("bytes")).over(windowSpec)
+
+// Calculate the maximum timestamp and minimum timestamp in the window
+val maxTimestamp = last(unix_timestamp(col("timestamp"))).over(windowSpec)
+val minTimestamp = first(unix_timestamp(col("timestamp"))).over(windowSpec)
+
+// Calculate the interval in seconds
+val intervalSeconds = maxTimestamp - minTimestamp
+
+// Calculate the difference between 5th record's bytes and 1st record's bytes
+val bytesDifference = fifthBytes - firstBytes
+
+// Select the required columns
+val resultDF = df
+  .select(col("timestamp"), bytesDifference.alias("BytesDifference"), intervalSeconds.alias("IntervalSeconds"))
+  .filter(intervalSeconds.isNotNull)
+
+// Show or perform further operations on resultDF as needed
+resultDF.show()
